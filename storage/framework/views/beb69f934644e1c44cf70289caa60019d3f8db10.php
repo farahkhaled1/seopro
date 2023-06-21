47c52af78a40b8970a581d0b5aa0b70b1a9d34a1
Farah Khaled <farah1900784@miuegypt.edu.eg>
2023-06-21 12:45:33 +0300

    email template

--- storage/framework/views/beb69f934644e1c44cf70289caa60019d3f8db10.php
+++ storage/framework/views/beb69f934644e1c44cf70289caa60019d3f8db10.php
@@ -8,8 +8,28 @@
     <title>SEOPro</title>
 </head>
 <body>
-    <h1>SEOPro </h1>
-    <h3><?php echo e($data['body']); ?></h3>
-    
+    <h1>SEOPro</h1>
+    <h3><?php echo nl2br(e($data['body'])); ?></h3>
 </body>
-</html><?php /**PATH /Users/farahkhaled/Desktop/seopro-1/resources/views/emails/index.blade.php ENDPATH**/ ?>
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f5f5f5;
+    }
+
+    h1 {
+        color: #333333;
+        margin-top: 0;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+
+    li {
+        margin-bottom: 10px;
+    }
+</style>
+</html>
+<?php /**PATH /Users/farahkhaled/Desktop/seopro-1/resources/views/emails/index.blade.php ENDPATH**/ ?>
